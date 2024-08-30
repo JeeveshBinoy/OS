75ea31a2df9374c98a017c2232b61382fc9d8bfa
Jeevesh Binoy <151763733+JeeveshBinoy@users.noreply.github.com>
2024-08-30 10:13:26 +0530

    lru.c

--- lru.c
+++ lru.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
-void main(){
-    int i,j,k,n,count=0,m[25],used[10]={0},rs[50],f,pf=0;
-    printf("Enter the length : ");
-    scanf("%d",&n);
-    printf("Reference string : ");
-    for(i=0;i<n;i++)
-    scanf("%d",&rs[i]);
-    printf("Frames : ");
-    scanf("%d",&f);
-    
-    for(i=0;i<f;i++)
-    m[i]=-1;
-    
+
+void main() {
+    int i, j, k, n, f, pf = 0;
+    int m[25], used[25] = {0}, rs[50];
+
+    printf("Enter the length: ");
+    scanf("%d", &n);
+
+    printf("Reference string: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &rs[i]);
+    }
+
+    printf("Frames: ");
+    scanf("%d", &f);
+
+    for (i = 0; i < f; i++) {
+        m[i] = -1;
+        used[i] = -1;
+    }
+
     printf("Page Replacement\n");
-    for(i=0;i<n;i++){
-        for(k=0;k<f;k++){
-            if(m[k]==rs[i]){
-                used[k]=i+1;
+    
+    for (i = 0; i < n; i++) {
+        for (k = 0; k < f; k++) {
+            if (m[k] == rs[i]) {
+                used[k] = i; 
                 break;
             }
         }
-        
-        if(k==f){
-            int mintime=used[0],minindex=0;
-            for(j=1;j<f;j++){
-                if(used[j]<mintime){
-                    mintime=used[j];
-                    minindex=j;
+
+        if (k == f) {
+            int minIndex = 0;
+            for (j = 1; j < f; j++) {
+                if (used[j] < used[minIndex]) {
+                    minIndex = j;
                 }
             }
-            m[minindex]=rs[i];
-            used[minindex]=i+1;
+            m[minIndex] = rs[i];
+            used[minIndex] = i;
             pf++;
-            
+        }
+
+        for (j = 0; j < f; j++) {
+            if (m[j] != -1) {
+                printf("%d\t", m[j]);
+            } else {
+                printf("-\t");
             }
-            
-            for(j=0;j<n;j++)
-            printf("%d\t",m[j]);
-            
-            printf("\n");
+        }
+        printf("\n");
     }
-    printf("Total page faults: %d",pf);
+
+    printf("Total page faults: %d\n", pf);
 }
